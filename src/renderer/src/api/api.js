386522e83f9c0bcd896aef9db2a1e111f31ee286
Raunak <raunakdob@gmail.com>
2026-01-16 17:29:28 +0530

    config: Update auto-update URL and refine environment variable settings for socket connection and apply code formatting.

--- src/renderer/src/api/api.js
+++ src/renderer/src/api/api.js
@@ -1,20 +1,21 @@
-import axios from "axios";
+import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-});
-console.log("API base URL:", instance.defaults.baseURL);
+  baseURL: import.meta.env.VITE_BASE_URL
+})
+
+console.log('API base URL:', instance.defaults.baseURL)
 instance.interceptors.request.use((config) => {
   // Ensure headers exists
-  config.headers = config.headers ?? {};
+  config.headers = config.headers ?? {}
 
   // Add token if available
-  const token = sessionStorage.getItem("token");
+  const token = sessionStorage.getItem('token')
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
 
-  return config;
-});
+  return config
+})
 
-export default instance;
+export default instance
